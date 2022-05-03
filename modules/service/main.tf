c130c247f8a1af258b7adb07cbd0148ec74f8b08
Adam Escamilla <AdamEscamilla@users.noreply.github.com>
2022-05-03 01:12:44 -0500

    disable dependant services

--- modules/service/main.tf
+++ modules/service/main.tf
@@ -3,4 +3,5 @@ resource "google_project_service" "default" {
         service = var.service_name
 	disable_dependent_services = var.disable_dep_services
 	disable_on_destroy = var.disable_on_destroy
+        disable_dependent_services = true
 }
